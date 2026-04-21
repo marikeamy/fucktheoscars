731e21d418182e8a3a48714f0e3481bfde9fc14f
Marike Platen <marike.platen@heig-vd.ch>
2026-04-21 14:39:57 +0200

    deal with data to get every movie by decade (data.js) and add statues.js to implement the third page

--- src/main.js
+++ src/main.js
@@ -1,23 +1,33 @@
 import './style.css' 
+import { getData, getMoviesByDecade } from './data.js'
+import { drawStatues } from './statues.js'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 
 //Enregistrer le plugin.
 //Gsap utilise un système de plug-ins opt-ing, 
-// il faut préciser qu'il faut activer ScrollTrigger pour l'utiliser.
+//il faut préciser qu'il faut activer ScrollTrigger pour l'utiliser.
 gsap.registerPlugin(ScrollTrigger)
 
 //L'élément qui se déplacera
-const element = document.querySelector('.horizontal-wrapper');
+const horizontalWrapper = document.querySelector('.horizontal-wrapper');
 
-gsap.to(element, {     
+//Gestion du scroll horizontal
+gsap.to(horizontalWrapper, {     
     //x = de combien on déplace horizontalement                                                                                                                             
     x: -window.innerWidth,                                                                                                                                        
     scrollTrigger: {                                                                                                                                  
-      trigger: element,
+      trigger: horizontalWrapper,
       pin: true,                                                                                                                                      
       scrub: true,
       //end définit combien de scroll vertical déclenche l'animation.
       end: "+=" + (window.innerWidth)                                                                                                                                
     }             
-  })
+})
+
+const main = async () => {                                                                                                                          
+      const uniqueMovies = await getData()                                                                                                            
+      const decades = getMoviesByDecade(uniqueMovies)                                                                                                 
+      drawStatues(decades)                                                                                                                            
+  }               
+  main()
